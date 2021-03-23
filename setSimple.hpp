5c4fbd805327b45b08443df0dc6c57e6f1efaadb
Mikołaj Skałka <mikolaj.skalka@student.ujedu.pl>
2021-03-23 16:53:54 +0100

    Add methods definitions

--- setSimple.hpp
+++ setSimple.hpp
@@ -12,13 +12,17 @@ public:
 
 public:
     setSimple() : simpleSet(){};
+    setSimple(const setSimple& other);
+    setSimple(setSimple&& other);
     ~setSimple() = default;
 
     setSimple operator+(const setSimple &other);
     setSimple operator*(const setSimple &other);
     setSimple operator-(const setSimple &other);
-    setSimple operator==(const setSimple &other);
     setSimple &operator=(const setSimple &other);
+    setSimple &operator=(setSimple &&other);
+    bool operator==(const setSimple &other);
+    bool operator!=(const setSimple &other);
 
     setSimple sum(const setSimple &other);
 
@@ -31,11 +35,77 @@ public:
 };
 
 template <std::size_t N>
+setSimple<N>::setSimple(const setSimple &other)
+{
+    for(size_t i = 0; i < (N + IntBits - 1) / IntBits; i++ )
+    {
+        simpleSet[i / IntBits] = other.simpleSet[i / IntBits];
+    }
+}
+
+template <std::size_t N>
+setSimple<N>::setSimple(setSimple &&other)
+{
+    for(size_t i = 0; i < (N + IntBits - 1) / IntBits; i++ )
+    {
+    simpleSet[i / IntBits] = other.simpleSet[i / IntBits];
+    }
+}
+
+template <std::size_t N>
 setSimple<N> &setSimple<N>::operator=(const setSimple &other)
 {
-    simpleSet = other.simpleSet;
+    for(size_t i = 0; i < (N + IntBits - 1) / IntBits; i++ )
+    {
+        simpleSet[i / IntBits] = other.simpleSet[i / IntBits];
+    }
     return *this;
 }
+
+template <std::size_t N>
+setSimple<N> &setSimple<N>::operator=(setSimple &&other)
+{
+    for(size_t i = 0; i < (N + IntBits - 1) / IntBits; i++ )
+    {
+        simpleSet[i / IntBits] = other.simpleSet[i / IntBits];
+    }
+    return *this;
+}
+
+
+template <std::size_t N>
+setSimple<N> setSimple<N>::operator-(const setSimple &other)
+{
+    setSimple<N> output;
+    for(size_t i = 0; i < N; i++)
+    {
+        output.simpleSet[i / IntBits] |= (((simpleSet[i / IntBits])) & (~(other.simpleSet[i / IntBits])));
+    }
+    return output;
+}
+
+template <std::size_t N>
+setSimple<N> setSimple<N>::operator+(const setSimple &other)
+{
+    setSimple<N> output;
+    for (size_t i = 0; i < N; i++)
+    {
+        output.simpleSet[i / IntBits] |= (((simpleSet[i / IntBits])) | ((other.simpleSet[i / IntBits])));
+    }
+    return output;
+}
+
+template <std::size_t N>
+setSimple<N> setSimple<N>::operator*(const setSimple &other)
+{
+    setSimple<N> output;
+    for (size_t i = 0; i < N; i++)
+    {
+        output.simpleSet[i / IntBits] |= (((simpleSet[i / IntBits])) & ((other.simpleSet[i / IntBits])));
+    }
+    return output;
+}
+
 
 template <std::size_t N>
 setSimple<N> setSimple<N>::sum(const setSimple &other)
@@ -51,13 +121,25 @@ setSimple<N> setSimple<N>::sum(const setSimple &other)
 template <std::size_t N>
 void setSimple<N>::remove(const std::size_t elem)
 {
-    // simpleSet[elem / IntBits] |= (1U << (elem % IntBits))
+    simpleSet[elem / IntBits] &= ~(1U << (elem % IntBits));
 }
 
 template <std::size_t N>
 void setSimple<N>::insert(const std::size_t elem)
 {
     simpleSet[elem / IntBits] |= (1U << (elem % IntBits));
+}
+
+template <std::size_t N>
+bool setSimple<N>::contains(const std::size_t elem)
+{
+    return simpleSet[elem / IntBits] >> (elem % IntBits) & 1U;
+}
+
+template <std::size_t N>
+bool setSimple<N>::operator==(const setSimple<N> &other)
+{
+    return false;
 }
 
 template <std::size_t N>
